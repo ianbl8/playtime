f08eea2596301a624ed67e46a0471c31a9c97f1e
Ian Blake <ian@ianbl8.com>
2023-02-06 17:28:12 +0000

    Introduce sessions and authentication

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -30,12 +30,23 @@ export const accountsController = {
       if (!user || user.password !== password) {
         return h.redirect("/");
       }
+      request.cookieAuth.set({ id: user._id });
       return h.redirect("/dashboard");
     },
+
   },
   logout: {
     handler: function (request, h) {
       return h.redirect("/");
     },
   },
+
+  async validate(request, session) {
+    const user = await db.userStore.getUserById(session.id);
+    if (!user) {
+      return { isValid: false };
+    }
+    return { isValid: true, credentials: user };
+  },
+
 };
