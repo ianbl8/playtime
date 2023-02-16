6e022cf022acd85a3c0aeaa6bf2a8a170d524a64
Ian Blake <ian@ianbl8.com>
2023-02-16 21:57:24 +0000

    Install MongoDB and Mongoose

--- src/models/db.js
+++ src/models/db.js
@@ -3,10 +3,14 @@ import { userMemStore } from "./mem/user-mem-store.js";
 import { playlistMemStore } from "./mem/playlist-mem-store.js";
 import { trackMemStore } from "./mem/track-mem-store.js";
 
-// JsonStroe imports
+// JsonStore imports
 import { userJsonStore } from "./json/user-json-store.js";
 import { playlistJsonStore } from "./json/playlist-json-store.js";
 import { trackJsonStore } from "./json/track-json-store.js";
+
+// MongoDB imports
+import { connectMongo } from "./mongo/connect.js";
+import { userMongoStore } from "./mongo/user-mongo-store.js";
 
 export const db = {
   userStore: null,
@@ -20,7 +24,13 @@ export const db = {
         this.playlistStore = playlistJsonStore;
         this.trackStore = trackJsonStore;
         break;
-      default:
+        case "mongo":
+          this.userStore = userMongoStore;
+          // this.playlistStore = playlistMongoStore;
+          // this.trackStore = trackMongoStore;
+          connectMongo();
+          break;
+        default:
         this.userStore = userMemStore;
         this.playlistStore = playlistMemStore;
         this.trackStore = trackMemStore;
--- src/server.js
+++ src/server.js
@@ -52,7 +52,7 @@ async function init() {
   });
   server.auth.default("session");
 
-  db.init();
+  db.init("mongo"); // options: mongo, json (default: mem)
   server.route(webRoutes);
   await server.start();
   console.log("Server running on %s", server.info.uri);
