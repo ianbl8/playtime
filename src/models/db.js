5d9733e306f536f7756cde2c36a1dc71dc705308
Ian Blake <ian@ianbl8.com>
2023-02-16 15:21:08 +0000

    Update tests for memory stores

--- src/models/db.js
+++ src/models/db.js
@@ -1,7 +1,9 @@
-// import { userMemStore } from "./mem/user-mem-store.js";
-// import { playlistMemStore } from "./mem/playlist-mem-store.js";
-// import { trackMemStore } from "./mem/track-mem-store.js";
+// MemStore imports
+import { userMemStore } from "./mem/user-mem-store.js";
+import { playlistMemStore } from "./mem/playlist-mem-store.js";
+import { trackMemStore } from "./mem/track-mem-store.js";
 
+// JsonStroe imports
 import { userJsonStore } from "./json/user-json-store.js";
 import { playlistJsonStore } from "./json/playlist-json-store.js";
 import { trackJsonStore } from "./json/track-json-store.js";
@@ -11,9 +13,17 @@ export const db = {
   playlistStore: null,
   trackStore: null,
 
-  init() {
-    this.userStore = userJsonStore;
-    this.playlistStore = playlistJsonStore;
-    this.trackStore = trackJsonStore;
+  init(storeType) {
+    switch (storeType) {
+      case "json":
+        this.userStore = userJsonStore;
+        this.playlistStore = playlistJsonStore;
+        this.trackStore = trackJsonStore;
+        break;
+      default:
+        this.userStore = userMemStore;
+        this.playlistStore = playlistMemStore;
+        this.trackStore = trackMemStore;
+    }
   },
 };
