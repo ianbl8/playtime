2de4aca7a9fe2d9c92200602252f9b884d55b892
Ian Blake <ian@ianbl8.com>
2023-02-28 21:22:59 +0000

    Created initial user api

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import path from "path";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import { webRoutes } from "./web-routes.js";
+import { apiRoutes } from "./api-routes.js";
 import { db } from "./models/db.js";
 import { accountsController } from "./controllers/accounts-controller.js";
 
@@ -54,6 +55,7 @@ async function init() {
 
   db.init("mongo"); // options: mongo, json (default: mem)
   server.route(webRoutes);
+  server.route(apiRoutes);
   await server.start();
   console.log("Server running on %s", server.info.uri);
 }
